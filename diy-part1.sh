000310749439dfa682dff18a8a7da53955eae455
cwh1188 <cwh1188@163.com>
2026-09-03 16:51:54 +0800

    Cleanup unused patches in diy-part1.sh
    Removed unused patches for x86 and IEI WT61P803 hardware.

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# Remove unused x86 patch
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 rm -f target/linux/x86/patches/501-add-dmi-info-for-some-x86-devices.patch
+
+
+#
+# Remove unused IEI WT61P803 PUZZLE MCU patches
+#
+# These patches are for IEI PUZZLE hardware,
+# not for Linksys WRT1900ACS.
+# They also cause the current Linux 6.6 patch conflict.
+#
+
+find target/linux/mvebu/patches-6.6 \
+    -type f \
+    -iname '*wt61p803*puzzle*.patch' \
+    -delete
